93d750b3927fd158801591166fffd59d53f6be98
Iamouma <stephenouma452@gmail.com>
2025-01-04 15:22:44 +0300

    Added a few images to test the site, implemented the product_detail pag and add to cart functionality

--- walky_project/shop/urls.py
+++ walky_project/shop/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('products/<str:category>/', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
 ]
--- walky_project/shop/views.py
+++ walky_project/shop/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Product, Cart
 
 def homepage(request):
     return render(request, 'shop/homepage.html')
@@ -9,3 +11,22 @@ def product_list(request, category):
     products = Product.objects.filter(category=category)
     return render(request, 'shop/product_list.html', {'products': products, 'category': category})
 
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+@login_required
+def add_to_cart(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, id=product_id)
+        cart_item, created = Cart.objects.get_or_create(user=request.user, product=product)
+        if not created:
+            cart_item.quantity += 1
+        cart_item.save()
+        return JsonResponse({'message': 'Product added to cart!', 'quantity': cart_item.quantity})
+
+@login_required
+def view_cart(request):
+    cart_items = Cart.objects.filter(user=request.user)
+    total_price = sum(item.total_price() for item in cart_items)
+    return render(request, 'shop/cart.html', {'cart_items': cart_items, 'total_price': total_price})
